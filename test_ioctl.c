302da12cc430ed281a4533c9180147477f80b8e9
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2017-04-14 22:28:21 +0800

    Update test_ioctl.c

--- test_ioctl.c
+++ test_ioctl.c
@@ -4,20 +4,21 @@
 #include <sys/ioctl.h>  
 //needed for IO things. Attention that this is different from kernel mode 
 int lcd; 
-struct dev_msg_t {
-  char data[100];
+struct dev_message {
+  char data[20];
 };
 #define LCD_IOC_MAGIC  'k' 
 #define LCD_IOC_HELLO _IO(LCD_IOC_MAGIC,   1) 
-#define LCD_IOC_WRITE _IOW(LCD_IOC_MAGIC, 2, struct dev_msg_t)
-#define LCD_IOC_READ _IOR(LCD_IOC_MAGIC, 3, struct dev_msg_t)
-#define LCD_IOC_WR _IOWR(LCD_IOC_MAGIC, 4, struct dev_msg_t)
+#define LCD_IOC_WRITE _IOW(LCD_IOC_MAGIC, 2, struct dev_message)
+#define LCD_IOC_READ _IOR(LCD_IOC_MAGIC, 3, struct dev_message)
+#define LCD_IOC_WR _IOWR(LCD_IOC_MAGIC, 4, struct dev_message)
 
 void test()
 { 
   int k, i, sum; 
   char s[3]; 
-  struct dev_msg_t dev_msg;
+  struct dev_message dev_msg;
+  struct dev_message usr_msg;
   memset(dev_msg.data, 0, sizeof(dev_msg.data));
   memset(s, '2', sizeof(s)); 
   printf("test begin!\n"); 
@@ -25,25 +26,23 @@ void test()
   k = write(lcd, s, sizeof(s)); 
   printf("written = %d\n", k); 
   k = ioctl(lcd, LCD_IOC_HELLO); 
-  printf("result = %d\n", k); 
+  printf("Hello: k = %d\n", k); 
 
-  strcpy(dev_msg.data, "ioctl_write");
+  strcpy(dev_msg.data, "ioctl_write_dev_msg");
   k = ioctl(lcd, LCD_IOC_WRITE, &dev_msg);
-  printf("write result = %d msg = %s\n", k, dev_msg.data);
+  printf("IOC_WRITE: k = %d dev_msg = %s\n", k, dev_msg.data);
 
-  memset(dev_msg.data, 0, sizeof(dev_msg.data));
-  printf("cleara msg = %s\n", dev_msg.data);
-  k = ioctl(lcd, LCD_IOC_READ, &dev_msg);
-  printf("read result = %d msg = %s\n", k, dev_msg.data);
-
-  strcpy(dev_msg.data, "ioctl_exchange");
-  printf("before exchange msg = %s\n", dev_msg.data);
+  memset(usr_msg.data, '_', sizeof(dev_msg.data));
+  k = ioctl(lcd, LCD_IOC_READ, &usr_msg);
+  printf("IOC_READ: k = %d user_msg = %s\n", k, usr_msg.data);
+  
+  memset(usr_msg.data, '_', sizeof(usr_msg.data));
+  strcpy(dev_msg.data, "ioctl_msg_IOC_WR");
+  //printf("before exchange msg = %s\n", dev_msg.data);
   k = ioctl(lcd, LCD_IOC_WR, &dev_msg);
-  printf("exchange result = %d msg = %s\n", k, dev_msg.data);
+  strcpy(user_msg.data, dev_msg.data); 
+  printf("IOC_WR: k = %d user_msg = %s\n", k, user_msg.data);
 
-  memset(dev_msg.data, 0, sizeof(dev_msg.data));
-  k = ioctl(lcd, LCD_IOC_READ, &dev_msg);
-  printf("read result = %d msg = %s\n", k, dev_msg.data);
 } 
 
 int main(int argc, char **argv) 
