cc66a9e40d02ec4d04c13b12760aa3cd6fdde05b
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2021-05-11 23:41:39 +0800

    removed not used part.

--- src/ClosureInstance.py
+++ src/ClosureInstance.py
@@ -324,25 +324,6 @@ d['x'] = 23
 print(d['x'])
 
 
-#-----------------------------------------------------------------------------
-# Calling a method on an Object Given the Name as a String
-
-import math
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x 
-        self.y = y
-
-    def __repr__(self):
-        return 'Point({!r}, {!r})'.format(self.x, self.y)
-
-    def distance(self, x, y):
-        return math.hypot(self.x-x, self.y-y)
-
-pt = Point(2,3)
-dist = getattr(p, 'distance')(0, 0)
-print(dist)
 
 
 
