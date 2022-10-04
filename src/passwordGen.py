0b8c6745f2c84be519b9791ea3e0eee6d5593837
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2022-10-04 15:43:56 +0800

    Improve the password generate function.

--- src/passwordGen.py
+++ src/passwordGen.py
@@ -8,10 +8,9 @@ print(secure_str)
 # Output QQkABLyK
 
 # secure password
-password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(8)))
+password = ''.join((secrets.choice(string.ascii_letters +
+                                   string.digits + string.punctuation) for i in range(8)))
 print(password)
-
-
 
 
 message = password
@@ -19,7 +18,7 @@ message_bytes = message.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 base64_message = base64_bytes.decode('ascii')
 
-print("base64 encode: %s" %base64_message)
+print("base64 encode: %s" % base64_message)
 
 base64_bytes = base64_message.encode('ascii')
 message_bytes = base64.b64decode(base64_bytes)
@@ -29,11 +28,34 @@ print(message)
 
 
 def getRandomPasswd(pwdlength=8):
-    password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(pwdlength)))
+    password = ''.join((secrets.choice(string.ascii_lowercase + string.ascii_uppercase +
+                                       string.digits + string.punctuation) for i in range(pwdlength)))
     passwordBytes = password.encode('ascii')
     base64_bytes = base64.b64encode(passwordBytes)
     passwordMsg = base64_bytes.decode('ascii')
     return (password, passwordMsg)
+
+
+def RanPasswdGenerate(pwdlength=8, upCase=True, loCase=True, digit=True, spChar=True):
+    import string
+    import random
+    import secrets
+    charList = []
+    # Add the required charactor
+    if upCase:
+        charList.append(random.choice(string.ascii_uppercase))
+    if loCase:
+        charList.append(random.choice(string.ascii_lowercase))
+    if digit:
+        charList.append(random.choice(string.digits))
+    if spChar:
+        charList.append(random.choice(string.punctuation))
+    # Add a secret random string
+    charList.append(''.join((secrets.choice(string.ascii_lowercase + string.ascii_uppercase +
+                                            string.digits + string.punctuation) for _ in range(pwdlength-len(charList)))))
+    # random change the sequence
+    password = ''.join(random.sample(charList, len(charList)))
+    return password
 
 
 def passwdConvert(messageStr, b64Encode=True):
@@ -48,9 +70,12 @@ def passwdConvert(messageStr, b64Encode=True):
         message = message_bytes.decode('ascii')
         return message
 
+
 # test case:
 print('Start to test')
-password = ''.join((secrets.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(8)))
+password = ''.join((secrets.choice(string.ascii_lowercase + string.ascii_uppercase +
+                                   string.digits + string.punctuation) for i in range(6)))
+
 print(password)
 cvtStr = passwdConvert(password, b64Encode=True)
 print(cvtStr)
@@ -59,3 +84,6 @@ print(testPwd)
 
 result = 'pass' if password == testPwd else 'failed'
 print(result)
+
+
+print(RanPasswdGenerate(pwdlength=10))
