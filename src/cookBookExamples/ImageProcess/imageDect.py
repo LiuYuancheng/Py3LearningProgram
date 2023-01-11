7d9ae93255284a536816c1863d0d1c30037e41a1
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2023-01-11 15:25:21 +0800

    added the position markers.

--- src/cookBookExamples/ImageProcess/imageDect.py
+++ src/cookBookExamples/ImageProcess/imageDect.py
@@ -1,12 +1,28 @@
+
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        Desktop button detection .py
+#
+# Purpose:     This module is used to detect the button/switch base the windows desktop
+# 
+# Author:      Yuancheng Liu
+#
+# Version:     v_0.1
+# Created:     2022/01/11
+# Copyright:   n.a
+# License:     n.a
+#-----------------------------------------------------------------------------
+
+
 import cv2
 import numpy as np
 
-image= cv2.imread('1_1.png')
-cv2.imshow('Rainforest', image)
+image= cv2.imread('1.png')
+cv2.imshow('sreen short', image)
 cv2.waitKey(0)
 gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-template= cv2.imread('3_1.png',0)
+template= cv2.imread('3.png',0)
 
 
 result= cv2.matchTemplate(gray, template, cv2.TM_CCOEFF)
@@ -18,8 +34,20 @@ height, width= template.shape[:2]
 top_left= max_loc
 bottom_right= (top_left[0] + width, top_left[1] + height)
 
-cv2.rectangle(image, top_left, bottom_right, (0,0,255),5)
+cv2.rectangle(image, top_left, bottom_right, (0,0,255),3)
+font = cv2.FONT_HERSHEY_SIMPLEX
+  
+# org
+org = (top_left[0], top_left[1]-10)
+  
+# fontScale
+fontScale = 1
 
-cv2.imshow('Rainforest', image)
+pos_XY = (int((top_left[0]+bottom_right[0])/2), int((top_left[1]+bottom_right[1])/2))
+
+image = cv2.putText(image, 'find match: at %s' %str(pos_XY), org, font, 
+                   fontScale, (0,0,255), 2, cv2.LINE_AA)
+
+cv2.imshow('find match', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
