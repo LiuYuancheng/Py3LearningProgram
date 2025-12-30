b8b60480c07c3d9e84e494f02c4ce491df2e5fc3
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2025-12-30 17:16:39 +0800

    update the CTF hardware read me file.

---
+++ src/CTF_hardward/readme.md
@@ -0,0 +1,8 @@
+Company can help develop the hardware CTF.
+
+https://starlabs.sg/
+
+https://starlabs.sg/blog/2024/07-badgelife-at-off-by-one-conference-2024/
+
+https://starlabs.sg/blog/2025/05-badge-lanyard-challenge-at-obo-2025/
+
--- src/heatmap/heatmapTest.py
+++ src/heatmap/heatmapTest.py
@@ -1,9 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image 
+import scipy.ndimage as sp
 
-a = np.random.random((16, 16))
+a = np.random.random((24, 36))
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        if a[i][j] < 0.5:
+            a[i][j] = 0
+
 plt.imshow(a, cmap='hot', interpolation='nearest')
+
+#plt.imshow(a, origin='lower', cmap='hot', interpolation='bilinear')
+
 #plt.show()
 #plt.savefig('foo.png')
 plt.axis('off')
@@ -11,8 +20,18 @@ plt.axis('off')
 plt.savefig('foo.png', transparent=True, bbox_inches='tight')
 
 
+# x = [i[0] for i in a ]
+# y = [i[1] for i in a ]
+# X = sp.filters.gaussian_filter(x, sigma = 2, order = 0)
+# Y = sp.filters.gaussian_filter(y, sigma = 2, order = 0)
 
 
+# heatmap, xedges, yedges = np.histogram2d(X, Y, bins=40)
+# extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+# fig1 = plt.subplot(2,2,2)
+# plt.imshow(heatmap, extent=extent)
+# plt.savefig('foo2.png', transparent=True, bbox_inches='tight')
   
 img = Image.open('foo.png') 
 rgba = img.convert("RGBA") 
